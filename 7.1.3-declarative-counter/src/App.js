18fc3660043f06f72bbde36a617e3395aa652dde
Yugma Prajapati <yugmaprajapati0210@gmail.com>
2022-12-19 22:20:09 -0500

    changed the react app

---
+++ 7.1.3-declarative-counter/src/App.js
@@ -0,0 +1,18 @@
+//import React from 'react'
+import { useState } from 'react'
+
+import React, { useState } from 'react'
+
+function App(){
+    let [ counter, changeCounter ] = useState(1)
+    window.changeCounter = changeCounter
+    return (
+        <div>
+        <h1>{counter}</h1>
+        <h4>{counter}</h4>
+        </div>
+    )
+}
+
+export default App;
+
